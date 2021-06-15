b872b590afa36ec7e4685fb6b1d8874941fe40d6
Ted M. Young <tedyoung@gmail.com>
2021-06-15 06:36:41 -0700

    Changed isRed to use constant definition of "red" and added test.

---
+++ src/test/java/com/jitterted/ebp/blackjack/SuitTest.java
@@ -0,0 +1,21 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+// likely that this would be deleted as isRed is a constant in Suit
+class SuitTest {
+
+    @Test
+    public void diamondsAreRed() throws Exception {
+        assertThat(Suit.DIAMONDS.isRed())
+                .isTrue();
+    }
+
+    @Test
+    public void spadesAreNotRed() throws Exception {
+        assertThat(Suit.SPADES.isRed())
+                .isFalse();
+    }
+}
